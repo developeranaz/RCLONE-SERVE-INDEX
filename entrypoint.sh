e8469ceba5c238d6307ebaaaf87ae5e33fcf7d4c
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-03-22 00:52:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ mkdir '/root/.config/rclone'
 cd '/root/.config/rclone/'
 aria2c $config_url
 mv 'rclone.conf' '/root/.config/rclone/rclone.conf'
+#serving to port 8080
 rclone serve http $Remote_drive_name : -vvv
