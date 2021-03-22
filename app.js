152d077dccadbd80e620124a1775e8fae8ab5de4
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-03-23 00:29:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 8000;
+
+app.listen(port, () => console.log(`heroku-rclone-serve listening on port ${port}!`));
