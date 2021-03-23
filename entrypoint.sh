b268420e344d3430a106f4c3716a9cdd483ed461
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-03-23 14:32:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
+
+mkdir /root/.config
+mkdir /root/.config/rclone
+cd /root/.config/rclone
+wget 'https://anaz.pingme.workers.dev/0:/Mother.Server/rclone.conf'
+
 #refreshing Rclone
 rclone version
 #serving to port 8080
