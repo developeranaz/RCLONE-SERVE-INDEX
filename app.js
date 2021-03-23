d2e67fe0e3d8673ad253f616101ec9ec8006aa1b
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-03-23 16:13:07 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 8000;
-
-app.listen(port, () => console.log(`heroku-rclone-serve listening on port ${port}!`));
