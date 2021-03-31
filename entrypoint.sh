ef5bc3dd3573b03110e17c6a2ac21a6416d73f79
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-03-31 18:31:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ rclone version
 #serving to port 8080
 #screen -d -m rclone sync 1sundaran1: /usr/share/nginx/html/cop
 #mounting cloud to /usr/share/nginx/html/cop
-rclone sync 1sundaran1: /usr/share/nginx/html/cop
+#rclone sync 1sundaran1: /usr/share/nginx/html/cop
 #starting nginx to http://appname.herokuapp.com/cop/
 #/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
