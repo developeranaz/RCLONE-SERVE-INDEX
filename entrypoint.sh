e3be7521404fe8cb86ecda646fe3b97f7f77d14f
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-04-01 23:39:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 #refreshing Rclone
 rclone version
 #serving to port 8080
-rclone serve http 1sundaran1: --addr :$PORT
-#screen -d -m rclone sync 1sundaran1: /usr/share/nginx/html/cop
+rclone serve http $CLOUDNAME: --addr :$PORT
+#screen -d -m rclone sync 1su: /usr/share/nginx/html/cop
 #mounting cloud to /usr/share/nginx/html/cop
 #rclone sync 1sundaran1: /usr/share/nginx/html/cop
 #starting nginx to http://appname.herokuapp.com/cop/
