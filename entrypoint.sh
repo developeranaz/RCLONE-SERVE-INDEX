9b603f96f90fc64562e28d72e249927cbd17e869
Devill Don <53294521+devillD@users.noreply.github.com>
2021-06-20 11:37:59 +0600

    Added authentication
    User must have to login to access the index

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ rclone version
 rclone listremotes
 #refreshing Rclone
 wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
+wget $AUTH_FILE_URL -O authfile
 rclone version
 rclone listremotes
-rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
+rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M --htpasswd authfile
