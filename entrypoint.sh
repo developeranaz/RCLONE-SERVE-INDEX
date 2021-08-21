65e1c724c76fa68e990fe9d51cd53ac2b01fc149
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-22 00:49:36 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
-rc version
-wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
+mkdir /.config
+mkdir /.config/rclone/
+wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
+rclone version
 rc listremotes
 rc rcd --rc-serve --rc-addr=0.0.0.0:$PORT --rc-user=$INDEXusername --rc-pass=$INDEXpassword
