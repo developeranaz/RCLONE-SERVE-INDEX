539762ed478b3840a2afe9e83b7ad1a0e2a6f151
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-22 00:59:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 rclone version
 mkdir /.config
 mkdir /.config/rclone/
-wget -nc $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
+wget -nc $CONFIG_IN_URL -O '/.rclone.conf'
 rclone listremotes
 rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
