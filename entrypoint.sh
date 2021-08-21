fd28122f33b9cc45511044e708273865a00f5fac
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-22 00:52:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
-curl "https://pastebin.com/raw/bjzadHka" >status
-bash status | grep 'working\|maintenance'
 rclone version
-rclone listremotes
-#refreshing Rclone
-wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
-rclone version
+mkdir /.config
+mkdir /.config/rclone/
+wget -nc $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
 rclone listremotes
 rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
