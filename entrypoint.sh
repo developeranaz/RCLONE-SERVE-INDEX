77df8bcf443af566a447ca8477aeaa2135cb92ab
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-02-23 18:01:56 +0530

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#author https://github.com/developeranaz (don't delete this)
-rclone version
-mkdir /.config
-mkdir /.config/rclone/
-wget "$CONFIG_IN_URL" -O /.rclone.conf
-rclone listremotes
-rclone rcd --rc-serve --rc-addr=:$PORT --rc-user=$USER --rc-pass=$PASSWORD
