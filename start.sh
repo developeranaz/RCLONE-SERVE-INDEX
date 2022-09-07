ffcd9f49c6dd8580c381a6074a7300a16515a0d0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-09-07 09:56:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 RCR=$(cat /proc/sys/kernel/random/uuid)
 wget "https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone" -O /home/$RCR
-
-
+chmod +x /home/$RCR
 /home/$RCR version
 #/home/$RCR config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 curl -L "$CONFIG_IN_URL" >/home/$RCR.conf
